86118c26d2edb3186af5c132d5d0e02b001c5079
Anderson Costa <anderson_costa_miguel@hotmail.com>
2021-08-01 13:46:11 -0300

    calculadora desenvolvida em js, html, css

---
+++ aulas_js/aula01.js
@@ -0,0 +1,8 @@
+// var data1 = new Date(2020,0,1)
+// var data2 = new Date(2021, 0,1 )
+
+// var milisegundos_entre_datas = Math.abs(data1.getTime() - data2.getTime())
+// var milisegundos_por_dia = 1*24*60* 60 *1000
+
+// console.log(milisegundos_entre_datas / milisegundos_por_dia)
+
